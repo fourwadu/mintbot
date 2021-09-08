ddbb5b36b4c3a870860ae171bd197faeba3fde36
noah <noah.sigel@gmail.com>
2021-09-08 01:35:28 -0400

    nowrok

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-import { providers, Wallet } from "ethers";
+import { providers, Wallet, BigNumber, Contract } from "ethers";
 import { FlashbotsBundleProvider } from "@flashbots/ethers-provider-bundle";
+import web3 from "web3";
 const dotenv = require("dotenv");
 dotenv.config();
 
-const CHAIN_ID = 5;
+const CHAIN_ID = 1;
 const provider = new providers.InfuraProvider(CHAIN_ID);
-
-const FLASHBOTS_ENDPOINT = "https://relay-goerli.flashbots.net";
-
-if (process.env.WALLET_PRIVATE_KEY === undefined) {
-	console.error("Please provide WALLET_PRIVATE_KEY env");
-	process.exit(1);
-}
 
 const wallet = new Wallet(
 	"a9853f7e248167d9db0c8d1e0ed05b11fcbaa7816de337dc96ba91f88b8540bf",
@@ -19,41 +13,74 @@ const wallet = new Wallet(
 );
 
 // ethers.js can use Bignumber.js class OR the JavaScript-native bigint. I changed this to bigint as it is MUCH easier to deal with
-const gwei = 10n ** 9n;
-const ETHER = 10n ** 18n;
+const GWEI = BigNumber.from(10).pow(9);
+const PRIORITY_FEE = GWEI.mul(3);
 
 async function main() {
-	console.log((await wallet.getBalance()).toString());
-
 	const flashbotsProvider = await FlashbotsBundleProvider.create(
 		provider,
 		wallet,
 		`https://relay.flashbots.net`
 	);
 
-	provider.on("block", async (blockNumber) => {
-		const bundle = await flashbotsProvider.sendBundle(
+	provider.once("block", async (blockNumber) => {
+		const block = await provider.getBlock(blockNumber);
+
+		if (!block.baseFeePerGas) {
+			return;
+		}
+
+		const contract = new Contract(
+			"0x09151ae179692894c931276d546ca5a2f1907326",
 			[
 				{
-					transaction: {
-						chainId: CHAIN_ID,
-						type: 2,
-						value: 0,
-						data: "0x379607f50000000000000000000000000000000000000000000000000000000000000021",
-						maxFeePerGas: gwei * 3n,
-						maxPriorityFeePerGas: gwei * 2n,
-						// maxFeePerGas: 600n,
-						// maxPriorityFeePerGas: 1n,
-						to: "0x09151ae179692894c931276d546ca5a2f1907326",
-						gasLimit: 230000,
-					},
-					signer: wallet,
+					inputs: [
+						{
+							internalType: "uint256",
+							name: "tokenId",
+							type: "uint256",
+						},
+					],
+					name: "claim",
+					outputs: [],
+					stateMutability: "nonpayable",
+					type: "function",
 				},
-			],
+			]
+		);
+
+		var tx = await contract.populateTransaction.claim(web3.utils.toHex(4313));
+
+		const maxBaseFeeInFutureBlock =
+			FlashbotsBundleProvider.getMaxBaseFeeInFutureBlock(
+				block.baseFeePerGas,
+				1
+			);
+
+		const toBundle = [
+			{
+				transaction: {
+					chainId: 1,
+					type: 2,
+					value: 0,
+					maxFeePerGas: web3.utils.toHex(web3.utils.toWei("120", "gwei")),
+					maxPriorityFeePerGas: web3.utils.toHex(web3.utils.toWei("1", "gwei")),
+					gasLimit: web3.utils.toHex(web3.utils.toWei("21000", "gwei")),
+					// gasLimit: 30000000,
+					// maxPriorityFeePerGas: 1,
+					// maxFeePerGas: 101120125810,
+					...tx,
+				},
+				signer: wallet,
+			},
+		];
+
+		const bundle = await flashbotsProvider.sendBundle(
+			toBundle,
 			blockNumber + 1
 		);
 
-		// By exiting this function (via return) when the type is detected as a "RelayResponseError", TypeScript recognizes bundleSubmitResponse must be a success type object (FlashbotsTransactionResponse) after the if block.
+		// // By exiting this function (via return) when the type is detected as a "RelayResponseError", TypeScript recognizes bundleSubmitResponse must be a success type object (FlashbotsTransactionResponse) after the if block.
 		if ("error" in bundle) {
 			console.warn(bundle.error.message);
 			return;
